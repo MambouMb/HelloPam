0df3721700c0ad145f631b3eb4a4c9ce964593da
MAMBOU BETHEU FRANCK william <mwillgates@gmail.com>
2022-02-20 22:07:08 +0100

    Add project files.

---
+++ HelloPam.BO/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HelloPam.BO
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string  Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        public int Profile { get; set; }
+        public bool Statut { get; set; }
+        public byte[] Picture { get; set; }
+        public DateTime CreatedAt { get; set; }
+        
+
+        public User()
+        {
+
+        }
+
+        public User(int id, string username, string password, 
+                        string fullname, int profile, bool statut, byte[] picture,
+                        DateTime createdAt)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Fullname = fullname;
+            Profile = profile;
+            Statut = statut;
+            Picture = picture;
+            CreatedAt = createdAt;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is User user &&
+                   Id == user.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return 2108858624 + Id.GetHashCode();
+        }
+    }
+
+
+}
